2257de784c3ff566d9ac6278ca1b01d2998d5902
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-08-06 21:52:05 +0200

    rm example code

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
 Cypress.Commands.add("checkIfNo404", () => {
   cy.contains("Błąd 404").should("not.exist");
 });
+
 Cypress.Commands.add("visitPage", (path) => {
   cy.visit(`${Cypress.env("host")}${path}`);
 });
