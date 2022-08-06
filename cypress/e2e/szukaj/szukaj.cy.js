7e50e3bfc14acb7baf37e46f67ba1eea77a4bad0
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-08-06 22:31:24 +0200

    check results number in tab click

--- cypress/e2e/szukaj/szukaj.cy.js
+++ cypress/e2e/szukaj/szukaj.cy.js
@@ -9,16 +9,33 @@ describe("/szukaj", () => {
     cy.visitPage("/szukaj");
   });
 
-  it.skip("checks if no 404", () => {
+  it("checks if no 404", () => {
     cy.checkIfNo404();
     cy.contains("Wyniki wyszukiwania");
   });
 
   it.only("clicks the tabs", () => {
     // TODO remove classes selector and use e.g. data-cy="products-tab"
-    const CLASSES_SELECTOR = ".types-filter__link";
-    cy.get(CLASSES_SELECTOR).contains("Produkty").click();
-    cy.get(CLASSES_SELECTOR).contains("Inspiracje").click();
-    cy.get(CLASSES_SELECTOR).contains("Wszystko").click();
+    const TAB_SELECTOR = ".types-filter__link";
+    const RESULTS_SELECTOR = ".h4.fx";
+
+    cy.get(RESULTS_SELECTOR).contains(/wynik|wyniki|wyników/g);
+
+    cy.get(TAB_SELECTOR).contains("Inspiracje").click();
+    cy.get(RESULTS_SELECTOR).contains(/inspiracja|inspiracje|inspiracji/g);
+
+    cy.get(TAB_SELECTOR).contains("Produkty").click();
+    cy.get(RESULTS_SELECTOR).contains(/produkt|produkty|produktów/g);
+
+    cy.get(TAB_SELECTOR).contains("Wszystko").click();
+    cy.get(RESULTS_SELECTOR).contains(/wynik|wyniki|wyników/g);
+  });
+
+  it.only("checks for filters", () => {
+    ["Kategoria", "Polecane", "Przedział cenowy", "Marka"].forEach(
+      (tabName) => {
+        cy.get(".filter__name").contains(tabName);
+      }
+    );
   });
 });
